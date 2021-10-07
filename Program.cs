c1bec4d0dd672dab7a135a6615f5ebcf0b4e8838
sneakwish <wishal_wishnu@hotmail.com>
2021-10-07 12:16:53 +0200

    bestanden van hc

--- Program.cs
+++ Program.cs
@@ -1,22 +1,35 @@
 ﻿using System;
 using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace wpfwopdracht5
 {
 public class Grade {
-public int Id { get; set; }
-public int Value { get; set; }
+    public int Id { get; set; }
+    public int Value { get; set; }
+    public int StudentId { get; set; }
+
+    public Student Student { get; set; }
+
 }
 public class Student {
-public int Id { get; set; }
-public string Naam { get; set; }
-// public List<Grade> Grades { get; set; }
+    public int Id { get; set; }
+    [Required]
+    public string Naam { get; set; }
+    public Grade Grade { get; set; }
+
+
+    // public List<Grade> Grades { get; set; }
 }
 
 public class MyContext : DbContext {
-protected override void OnConfiguring(DbContextOptionsBuilder b) =>
-b.UseSqlite("Data Source=database.db");
-public DbSet<Student> Studenten { get; set; }
+    protected override void OnConfiguring(DbContextOptionsBuilder b) =>
+    b.UseSqlite("Data Source=database.db");
+    public DbSet<Student> Studenten { get; set; }
+    public DbSet<Grade> cijfers{get; set;}
 }
     class Program
     {
@@ -24,8 +37,20 @@ public DbSet<Student> Studenten { get; set; }
         {
             Console.WriteLine("Hello World!");
             MyContext c = new MyContext();
-            c.Studenten.Add(new Student(){ Id = 456, Naam = "Bob2"});
+            c.Studenten.Add(new Student(){Naam = "bole"});
+            c.Studenten.Add(new Student(){Naam = "bole"});
+            c.Studenten.Add(new Student(){Naam = "bole"});
+            c.Studenten.Add(new Student(){Naam = "bole"});
+            c.Studenten.Add(new Student(){Naam = "bole"});
+    
+            // c.Studenten.Single((s) => s.Id == 123).Grades.Add(new Grade);
+          
             c.SaveChanges();
+            
+            // Student st;
+            // var wishal = c.Studenten.Single(s=>s.Naam == "Wishal");
+            // c.Entry(wishal).Collection(st => st.Id).Load();
+            // Console.WriteLine(wishal);
             //yofjfh
         }
     }
